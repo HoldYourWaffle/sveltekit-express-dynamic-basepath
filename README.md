f98b3100660726afe4cece0ab7d9f5cd74b57f15
HoldYourWaffle <ravivanrooijen@live.nl>
2025-04-09 00:59:47 +0200

    Setup Express server

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sveltekit-express-dynamic-basepath
+
 Demo for integrating a SvelteKit app into an Express.js server with a dynamic base path.
+
+```sh
+pnpm install
+pnpm run start
+```
+
+On Windows you might need to enable [`shell-emulator`](https://pnpm.io/cli/run#shellemulator):
+
+```sh
+pnpm config set shell-emulator true
+```
---
+++ express/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { handler } from '../sveltekit/build/handler.js';
+
+// See: https://svelte.dev/docs/kit/adapter-node#Custom-server
+
+const app = express();
+
+// Add a route that lives separately from the SvelteKit app
+app.get('/healthcheck', (req, res) => {
+	res.end('ok');
+});
+
+// Delegate to SvelteKit
+app.use(handler);
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+});
